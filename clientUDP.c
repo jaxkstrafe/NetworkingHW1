4a74c6ad034315e1599875634b80227198807f3d
Jack Dalton <92889104+jaxkstrafe@users.noreply.github.com>
2024-02-12 18:42:26 -0600

    Add files via upload

---
+++ clientUDP.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVERPORT "8088" // the port users will be connecting to
+#define MAXDATASIZE 100 // max number of bytes the client can send
+
+int main(int argc, char *argv[]) {
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    int len;
+    char message[MAXDATASIZE];
+
+    if (argc != 3) {
+        fprintf(stderr,"usage: talker hostname message\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; //INET is IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+    
+    len = strlen(argv[2]);
+
+    //The client sends the message the user inputted into the terminal to 
+    //the server using sendto
+    //The message is read in using argv[2] as it is at index 2 and the 3rd argument
+    //inputted by the user
+    //Ex: ./a.out  <ip-address> <the message>
+    if((numbytes = sendto(sockfd, argv[2], len, 0, p->ai_addr,p->ai_addrlen)) == -1){
+    	perror("sendto");
+    	close(sockfd);
+    	exit(1);
+    }
+    
+    //If a client has successfully connect and found the sever it was looking for 
+    //it prints the following message
+    printf("client: connecting to %s\n", argv[1]);
+    
+    //The client then receives the captialzied message from the server using recvfrom
+    if((numbytes = recvfrom(sockfd, message, MAXDATASIZE - 1, 0, NULL, NULL)) == -1){
+    	perror("recvfrom");
+    	exit(1);
+    }
+    
+    message[numbytes] = '\0';
+    
+    //Prints the amount of bytes it recieved from the server
+    printf("client: recieved %d bytes\n", numbytes);
+    
+    //Prints the received message
+    printf("%s\n", message);
+    
+    //Close the socket
+    close(sockfd);
+
+    return 0;
+}
+
---
+++ serverUDP.c
@@ -0,0 +1,113 @@
+/*
+ * listener.c -- a datagram sockets "server" demo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <ctype.h>
+
+#define MYPORT "8088" // the port users will be connecting to
+#define MAXDATASIZE 100
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void) {
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char message[MAXDATASIZE];
+    socklen_t addr_len;
+    char s[INET_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    //After this line the server is now waiting to recieve a message from a client
+    printf("listener: waiting to recvfrom...\n");
+
+    //When a client sends a message to the server the server
+    //Receives the message from the client using recvfrom
+    //And stores the sent message in 'message'
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, message, MAXDATASIZE - 1 , 0,(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    //Prints the IP address it received the message from
+    printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
+    
+    message[numbytes] = '\0';
+    
+    //Prints the message it recieved from the client
+    printf("listener: recived client message\"%s\"\n", message);
+    
+    //Coverts the message from the client to all uppercase as per the instructions
+    //Using the c function toupper to change each letter to uppercase
+    for(int i = 0; i < numbytes; i++){
+    	message[i] = toupper(message[i]);
+    }
+    
+    //The server sends the now all uppercase message back to the client using sendto
+    if((numbytes = sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&their_addr, addr_len)) == -1){
+    	perror("sendto");
+    	exit(1);
+    }
+    
+    //Prints the number of bytes it sent to the client
+    //Which is just the num of bytes contained in the message
+    printf("listener: sent %d bytes back to the client\n", numbytes);
+
+    //Close the socket
+    close(sockfd);
+
+    return 0;
+}
+
